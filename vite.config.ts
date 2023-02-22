e0e1683d23e6e57fb630d9c5d11ae36f52c442e0
oushihao <oushihao@bytedance.com>
2023-02-22 19:38:22 +0800

    ci: github page
    Change-Id: I0ff008281a8c2a6e4d30017d156946277a0dcc09

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/use-useEffect-or-not/',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
